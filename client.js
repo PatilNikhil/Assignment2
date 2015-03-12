f61b7d5e982e3d05b968853f083f6cb72a233d88
Nikhil Patil <nikhil.patil@mindstix.com>
2015-03-12 17:20:35 +0530

    Corrections are done

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 /* Node module to demonstrate how to combine all 3 modules.
  @author Nikhil */
 
-var fileR = require("./readFile");
-var sortD = require("./sortData");
-var fileW= require("./writeFile")
-//calling read function
-fileR.read("source.json", function(err, data) {
+var rf = require("./readFile");
+var sd = require("./sortData");
+var wf = require("./writeFile");
+
+var fileRead = rf.fileRead;
+var sort = sd.sort;
+var fileWrite = wf.fileWrite;
+
+fileRead.read("source.json", function(err, data) {
 	if(err){
 		//displaying error if file not found
 		console.log(err);
@@ -13,19 +17,12 @@ fileR.read("source.json", function(err, data) {
 	else{
 		if(data != null){
 			//calling sort function
-			var result = sortD.sort(data);
+			var result = sort.sortArray(data);
 			//writing data in .txt file
-			fileW.writeTxt("txtFile.txt",result);
+			fileWrite.writeTxt("txtFile.txt",result);
 			//writing data in .xml file
-			fileW.writeXml("xmlFile.xml",result);
+			fileWrite.writeXml("xmlFile.xml",result);
 		}
 	}
 
 });
-
-/**
-1. It will be great if you can use javascript oops concept for create modules
-
-2. Use proper names for function params e.g. use callback instead call
-
-*/
